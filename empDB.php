cab3ae6f7ff84cd78efb2c0d855fa2f87748dfda
Omar Ahmed Salah <omaricka4lyfe@gmail.com>
2021-06-09 13:58:53 +0200

    admin frontend

---
+++ empDB.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Employee Database</title>
+    <?php include "adminNav.php";?>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <br><br><br>
+    <h1>Employee Database</h1>
+  </body>
+</html>
+
+<style media="screen">
+
+
+</style>
