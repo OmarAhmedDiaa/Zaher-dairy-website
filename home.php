1b93db2cc81c6eff232135da767398d74fde7bae
Omar Ahmed Salah <omaricka4lyfe@gmail.com>
2021-06-16 21:16:05 +0200

    home carousel

--- home.php
+++ home.php
@@ -15,33 +15,43 @@
       </div>
     </div>
     <img class="promo" src="pics/eid.jpg" alt="Eid Promotion">
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">1/2 Kg Cheddar Cheese</div>
-            <div class="panel-body"><img src="pics/cheddar.jpg" class="img-responsive" style="width:67%" alt="Image"></div>
-            <div class="panel-footer">50% OFF : 40 LE</div>
-          </div>
+    <br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br>
+    <br><br><br><br><br><br>
+    <h1>What we have to offer:</h1><br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" style="margin-left:575px;">
+        <div class="item active">
+          <img src="pics/1.jpg" width="25%" >
         </div>
-        <div class="col-sm-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">Rice Pudding</div>
-            <div class="panel-body"><img src="pics/roz.jpg" class="img-responsive" style="max-width:100%" alt="Image"></div>
-            <div class="panel-footer">15 LE</div>
-          </div>
+
+        <div class="item">
+          <img src="pics/2.jpg" width="25%">
         </div>
-        <div class="col-sm-4">
-          <div class="panel panel-primary">
-            <div class="panel-heading">Molten Cake</div>
-            <div class="panel-body"><img src="pics/molten.jfif" class="img-responsive" style="max-width:89%" alt="Image"></div>
-            <div class="panel-footer">30 LE</div>
-          </div>
+
+        <div class="item">
+          <img src="pics/3.jpg" width="25%">
         </div>
       </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <br>
   </body>
 </html>
 
@@ -56,15 +66,6 @@
     left: 50%;
     transform: translatex(-50%);
     max-width: 65%;
-}
-
-.panel-footer, .panel-heading{
-  text-align:center;
-}
-
-footer{
-  background-color: #5eaaa8;
-  color:white;
 }
 
 </style>
--- promotions.php
+++ promotions.php
@@ -12,5 +12,41 @@
   <body>
     <br><br><br>
     <h1>Sales & Promotions</h1>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">400 gm Al Duha Penne Rigate</div>
+        <div class="panel-body"><img src="pics/promos/macaroni.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer"><label style="text-decoration:line-through;">50 EGP</label> <label>25 EGP</label>  <br><br>
+          <button type="button" name="button">Add to Cart</button></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Ice-cream Tarte</div>
+        <div class="panel-body"><img src="pics/promos/tart.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer"><label style="text-decoration:line-through;">80 EGP</label> <label>50 EGP</label> <br><br>
+          <button type="button" name="button">Add to Cart</button></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">1/2 Kg Mainz German Cheese</div>
+        <div class="panel-body"><img src="pics/promos/mainz.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer"><label style="text-decoration:line-through;">70 EGP</label> <label>40 EGP</label> <br><br>
+          <button type="button" name="button">Add to Cart</button></div>
+      </div>
+    </div>
+  </div>
+</div><br>
   </body>
 </html>
+
+<style media="screen">
+button{
+  background-color: #F05945;
+  border-radius: 4px;
+  color:#fff;
+}
+</style>
