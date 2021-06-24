fdccb2b17cc40ab25a7fdbefe68028a36865363c
Omar Ahmed Salah <omaricka4lyfe@gmail.com>
2021-06-24 11:20:28 +0200

    confirmation

---
+++ confirmation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Confirmation</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="main.css">
+    <?php include "navbar.php"; ?>
+  </head>
+  <body>
+    <br><br><br>
+    <h2>Your order has been submitted!</h2><br>
+    <p>Your order will be delivered within 20-30 minutes! <br>Thank you for shopping at Zaher Dairy</p><br>
+    <a href="home.php" style="color:#F05945;">Return to Home Page</a><br><br>
+    <img class="social"src="pics/zaher.jpg" alt="Social Media Accounts">
+
+  </body>
+</html>
+
+<style media="screen">
+  .social{
+    width:30%;
+  }
+</style>
